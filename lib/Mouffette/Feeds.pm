039bbd2b2af96dce1701469f3353a75b556d8d85
Marco <marco@theanarchistlibrary.org>
2012-09-07 11:42:42 +0200

    Added the insert into gets + comments
      *  edited              lib/Mouffette/Feeds.pm

--- lib/Mouffette/Feeds.pm
+++ lib/Mouffette/Feeds.pm
@@ -23,37 +23,49 @@ use Mouffette::Utils qw/bot_fast_reply/;
 
 sub validate_feed {
   my ($con, $msg, $dbh, $handle, $url) = @_;
+  # sanity check
   unless ($handle and $url) {
     return bot_fast_reply($con, $msg, "Invalid arguments. See the help");
   }
-  my $jid = bare_jid($msg->from); # bare jid
-  # first, check if the url is already in the db;
-  my $sth =
+  # get the bare jid
+  my $jid = bare_jid($msg->from);
+
+  # owe queries
+  my $sthcheck =
     $dbh->prepare('SELECT handle, url FROM feeds WHERE url = ? or handle = ?;');
   my $sthfeed = $dbh->prepare('INSERT INTO feeds (handle, url) VALUES (?, ?);');
   my $sthassc = $dbh->prepare('INSERT INTO assoc (handle, jid) VALUES (?, ?);');
+  my $sthhttp = $dbh->prepare('INSERT INTO gets  (url) VALUES (?);');
 
-  $sth->execute($url, $handle);
-  if (my @feedrow = $sth->fetchrow_array()) {
+  $sthcheck->execute($url, $handle);
+  if (my $checkerror = $sthcheck->err) {
+    return bot_fast_reply($con, $msg, $checkerror);
+  }
+  # handle or url already present?
+  if (my @feedrow = $sthcheck->fetchrow_array()) {
     ($handle, $url) = @feedrow;
-    # in the next query we get an error if the same association is present.
-    # I don't care and keep saying that the dude is subscribed.
+    # yes? ok, add the association and notify.
     $sthassc->execute($handle, $jid);
     bot_fast_reply($con, $msg,
 		   "Feed already present: you're subscribed to " . 
 		   "$handle ($url) now");
   } else {
+    # fetch the feed. Everything is passed to the AnyEvent::HTTP
+    # closure. Who knows if we get leaks doing so
     http_get $url, sub {
       my ($data, $hdr) = @_;
+      # check if it's valid
       unless ($hdr->{Status} eq "200") {
 	return bot_fast_reply($con, $msg, "Feed failed: " . $hdr->{Reason});
       }
+      # and it parses cleanly
       my $feed = XML::Feed->parse(\$data) or
 	return bot_fast_reply($con, $msg, "Feed failed: " . XML::Feed->errstr);
       # ok, all seems valid.
       $sthfeed->execute($handle, $url);
       $sthassc->execute($handle, $jid);
-      if (my $error = $sthfeed->err || $sthassc->err) {
+      $sthhttp->execute($url);
+      if (my $error = $sthfeed->err || $sthassc->err || $sthhttp->err) {
 	bot_fast_reply($con, $msg, "errors: $error");
       } else {
 	bot_fast_reply($con, $msg,
