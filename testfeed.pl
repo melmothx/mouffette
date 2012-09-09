e734f03f82111cfe354a8b12ebdfa09c83a10d72
Marco <marco@theanarchistlibrary.org>
2012-09-09 17:42:39 +0200

    Added support for feed enclosures (nice for podcasts)

--- testfeed.pl
+++ testfeed.pl
@@ -5,11 +5,15 @@ use warnings;
 use lib './lib';
 use Mouffette::Feeds qw/xml_feed_parse/;
 use Data::Dumper;
-use LWP::Simple;
+use LWP::UserAgent;
 
 die "provide an url as argument\n" unless $ARGV[0];
 
-my $data = get $ARGV[0];
+my $ua = LWP::UserAgent->new(agent => "Mozilla");
+$ua->show_progress(1);
 
+my $data = $ua->get($ARGV[0])->decoded_content();
+
+# print $data;
 
 print Dumper(xml_feed_parse("test", \$data));
