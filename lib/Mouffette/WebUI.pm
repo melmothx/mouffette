501aa478f8b03edec8ac2315d946487d27243317
Marco <marco@theanarchistlibrary.org>
2012-09-10 18:36:19 +0200

    Added HTML output to WebUI. It seems to work
      *  edited              lib/Mouffette/WebUI.pm

--- lib/Mouffette/WebUI.pm
+++ lib/Mouffette/WebUI.pm
@@ -16,9 +16,42 @@ our @EXPORT_OK = qw(wi_report_status);
 
 our $VERSION = '0.01';
 
+use HTML::Entities;
+
 sub wi_report_status {
   my $dbh = shift;
-  return "Hello world!";
+
+  my @rows = ('<table id="feeds"><tr>',
+	      '<th>Alias</th>',
+	      '<th>Feed</th>',
+	      '<th>Subscribers</th>',
+	      '</tr>');
+
+  my $usercount = 0;
+  my $users = $dbh->prepare('SELECT DISTINCT jid FROM assoc');
+  $users->execute();
+  while ($users->fetchrow_array()) {
+    $usercount++;
+  };
+
+  my $feedcount = 0;
+  my $feeds =
+    $dbh->prepare('SELECT feeds.handle, feeds.title, feeds.url,
+   (SELECT COUNT (jid) FROM assoc WHERE feeds.handle = assoc.handle) AS count
+     FROM feeds ORDER BY title');
+
+  $feeds->execute();
+  while(my ($users, $title, $url, $followers) = $feeds->fetchrow_array()) {
+    push @rows, '<tr><td>' . encode_entities($users) . '</td><td><a href="'
+      . $url . '">' . encode_entities($title) . '</a></td><td>' . $followers
+	. '</td></tr>';
+    $feedcount++;
+  }
+  push @rows, '</tr></table>';
+
+  return '<div id="botstatus">'
+    . "<p>Serving $feedcount feeds to $usercount user!</p>"
+    . join("", @rows) . '</div>';
 }
 
 
